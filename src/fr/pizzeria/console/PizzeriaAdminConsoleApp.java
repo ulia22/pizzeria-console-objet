7ae44f51b7d5a79c11cf7e2085a66b6da279096c
ETY9 <ETY9@DESKTOP-1556SEK>
2017-09-21 17:46:51 +0200

    Mise en place de la javadoc.

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -5,32 +5,43 @@ import java.util.Scanner;
 
 import fr.pizzeria.model.Pizza;
 
+/**
+ * Classe contenant le point d'entré du programme, utilisant la console pour
+ * recevoir/envoyer les inputs/outputs du programme.
+ * 
+ * @author Clément B.
+ * 
+ */
 public class PizzeriaAdminConsoleApp {
 
+	/** Chaine de caractère contenant le menu. */
 	private static String menu = new String(
-			"***** Pizzeria Administration *****\n"
-			+ "1. Lister les pizzas\n"
-			+ "2. Ajouter une nouvelle pizza\n"
-			+ "3. Mettre à jour une pizza\n"
-			+ "4. Supprimer une pizza\n"
-			+ "99. Sortir\n");
-	
+			"***** Pizzeria Administration *****\n" + "1. Lister les pizzas\n" + "2. Ajouter une nouvelle pizza\n"
+					+ "3. Mettre à jour une pizza\n" + "4. Supprimer une pizza\n" + "99. Sortir\n");
+
+	/** Tableau contenant la liste des pizzas au menu. */
 	private static Pizza[] listePizza;
+	/** Scanner qui sera utilisé dans toute l'application */
 	public static Scanner sc = new Scanner(System.in);
-	
+
+	/**
+	 * Point d'entré de l'application.
+	 * 
+	 * @param args
+	 *            Non-utilisés dans le cadre de l'application.
+	 */
 	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
 		sc.useLocale(Locale.US);
 		String choice;
-		
+
 		PizzeriaAdminConsoleApp.initListePizza();
-		do{
+		do {
 			System.out.println(menu);
 			choice = sc.nextLine();
-			switch(choice){
+			switch (choice) {
 			case "1":
 				System.out.println("Liste des pizzas.");
-				for(Pizza p : PizzeriaAdminConsoleApp.listePizza){
+				for (Pizza p : PizzeriaAdminConsoleApp.listePizza) {
 					System.out.println(p.toString());
 				}
 				break;
@@ -49,18 +60,22 @@ public class PizzeriaAdminConsoleApp {
 			case "99":
 				System.out.println("Aurevoir \u2639");
 				break;
-				default:
-					System.out.println("Mauvaise entrée.");
+			default:
+				System.out.println("Mauvaise entrée.");
 				break;
-				
+
 			}
-		}while(!choice.equals("99"));
+		} while (!choice.equals("99"));
 		sc.close();
 	}
-	
-	private static void initListePizza(){
+
+	/**
+	 * Initialise le tableau contenant la liste des pizzas au menu avec les
+	 * pizzas par défaut.
+	 */
+	private static void initListePizza() {
 		PizzeriaAdminConsoleApp.listePizza = new Pizza[8];
-		
+
 		Pizza.setCurrentGlobalId(0);
 		PizzeriaAdminConsoleApp.listePizza[0] = new Pizza("PEP", "Pépéroni", 12.5);
 		PizzeriaAdminConsoleApp.listePizza[1] = new Pizza("MAR", "Margherita", 14.00);
@@ -71,89 +86,94 @@ public class PizzeriaAdminConsoleApp {
 		PizzeriaAdminConsoleApp.listePizza[6] = new Pizza("ORI", "L’orientale", 13.50);
 		PizzeriaAdminConsoleApp.listePizza[7] = new Pizza("IND", "L’indienne", 14.00);
 	}
-	
-	private static void addPizza(){
-		String code,nom;
+
+	/**
+	 * Permet d'ajout une pizza au menu.
+	 */
+	private static void addPizza() {
+		String code, nom;
 		double prix;
-		
+
 		System.out.println("Veuillez saisir le code");
 		code = sc.nextLine();
 		System.out.println("Veuillez saisir le nom (sans espace)");
 		nom = sc.nextLine();
 		System.out.println("Veuillez saisir le prix");
 		prix = Double.parseDouble(sc.nextLine());
-		
+
 		Pizza p = new Pizza(code, nom, prix);
-		Pizza[] tab = new Pizza[PizzeriaAdminConsoleApp.listePizza.length+1];
-		
-		for(int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++){
+		Pizza[] tab = new Pizza[PizzeriaAdminConsoleApp.listePizza.length + 1];
+
+		for (int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++) {
 			tab[i] = PizzeriaAdminConsoleApp.listePizza[i];
 		}
 		tab[PizzeriaAdminConsoleApp.listePizza.length] = p;
 		PizzeriaAdminConsoleApp.listePizza = tab;
 	}
-	
-	private static void updatePizza(){
+
+	/**
+	 * Permet de modifier une pizza déjà présente dans le menu.
+	 */
+	private static void updatePizza() {
 		String code;
-		
-		for(Pizza p : PizzeriaAdminConsoleApp.listePizza){
+
+		for (Pizza p : PizzeriaAdminConsoleApp.listePizza) {
 			System.out.println(p.toString());
 		}
 		System.out.println("Veuillez choisir la pizza à modifier.");
 		System.out.println("(99 pour abandonner).");
-		
-		
+
 		code = sc.nextLine();
-		if(!code.equals("99")){
+		if (!code.equals("99")) {
 			int index = 0;
-			for(int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++){
-				if(PizzeriaAdminConsoleApp.listePizza[i].getCode().equals(code)){
+			for (int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++) {
+				if (PizzeriaAdminConsoleApp.listePizza[i].getCode().equals(code)) {
 					index = i;
 				}
 			}
 			System.out.println("Veuillez saisir le code");
 			PizzeriaAdminConsoleApp.listePizza[index].setCode(sc.nextLine());
-			
+
 			System.out.println("Veuillez saisir le nom (sans espace)");
 			PizzeriaAdminConsoleApp.listePizza[index].setNom(sc.nextLine());
-			
+
 			System.out.println("Veuillez saisir le prix");
 			PizzeriaAdminConsoleApp.listePizza[index].setPrix(Double.parseDouble(sc.nextLine()));
-			
-			for(Pizza p : PizzeriaAdminConsoleApp.listePizza){
+
+			for (Pizza p : PizzeriaAdminConsoleApp.listePizza) {
 				System.out.println(p.toString());
 			}
 		}
-		
+
 	}
-	
+
 	/**
-	 * 
+	 * Permet de supprimer une pizza du menu.
 	 */
-	private static void deletePizza(){
+	private static void deletePizza() {
 		String code;
-		for(Pizza p : PizzeriaAdminConsoleApp.listePizza){
+		for (Pizza p : PizzeriaAdminConsoleApp.listePizza) {
 			System.out.println(p.toString());
 		}
 		System.out.println("Veuillez choisir la pizza à supprimer.");
 		System.out.println("(99 pour abandonner).");
-		
+
 		code = sc.nextLine();
-		if(!code.equals("99")){
-			//Trouverla pizza
+		if (!code.equals("99")) {
+			// Trouverla pizza
 			int index = 0;
-			for(int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++){
-				if(PizzeriaAdminConsoleApp.listePizza[i].getCode().equals(code)){
+			for (int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++) {
+				if (PizzeriaAdminConsoleApp.listePizza[i].getCode().equals(code)) {
 					index = i;
 				}
 			}
-			
-			//Del la pizza
+
+			// Del la pizza
 			PizzeriaAdminConsoleApp.listePizza[index] = null;
-			Pizza[] tab = new Pizza[PizzeriaAdminConsoleApp.listePizza.length-1];
+			Pizza[] tab = new Pizza[PizzeriaAdminConsoleApp.listePizza.length - 1];
 			int j = 0;
-			for(int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++){
-				if(PizzeriaAdminConsoleApp.listePizza[i] != null){
+			for (int i = 0; i < PizzeriaAdminConsoleApp.listePizza.length; i++) {
+				if (PizzeriaAdminConsoleApp.listePizza[i] != null) {
 					tab[j] = PizzeriaAdminConsoleApp.listePizza[i];
 					j++;
 				}
--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -1,92 +1,141 @@
 package fr.pizzeria.model;
 
+/** Classe représentant une pizza par instance. */
 public class Pizza {
-	
-private static int currentGlobalId;
-private int id;
-private String code;
-private String nom;
-private double prix;
 
-/**
- * @param code the code to set
- * @param nom the nom to set
- * @param prix the prix to set
- */
-public Pizza(String code, String nom, double prix) {
-	this.id = Pizza.currentGlobalId;
-	Pizza.currentGlobalId++;
-	this.code = code;
-	this.nom = nom;
-	this.prix = prix;
-}
+	/**
+	 * ID global des pizzas. Contient un ID unique libre à associer à la
+	 * prochaine instance de Pizza, lequel est incrémenté de 1 appres chaque
+	 * utilisation afin de rester unique.
+	 */
+	private static int currentGlobalId;
+	/** Id unique de l'instance courrante de Pizza. */
+	private int id;
+	/**
+	 * Code litteral d'identification de la pizza représenté par l'instance
+	 * courrante de Pizza.
+	 */
+	private String code;
+	/** Nom de la pizza représenté par l'instance courrante de Pizza. */
+	private String nom;
+	/** Prix de la pizza représenté par l'instance courrante de Pizza. */
+	private double prix;
 
-/* (non-Javadoc)
- * @see java.lang.Object#toString()
- */
-@Override
-public String toString() {
-	return this.getCode()+" -> "+this.getNom()+"("+this.getPrix()+"\u20AC)";
-}
-/**
- * @return the currentGlobalId
- */
-public static int getCurrentGlobalId() {
-	return currentGlobalId;
-}
-/**
- * @param currentGlobalId the currentGlobalId to set
- */
-public static void setCurrentGlobalId(int currentGlobalId) {
-	Pizza.currentGlobalId = currentGlobalId;
-}
-/**
- * @return the id
- */
-public int getId() {
-	return id;
-}
-/**
- * @param id the id to set
- */
-public void setId(int id) {
-	this.id = id;
-}
-/**
- * @return the code
- */
-public String getCode() {
-	return code;
-}
-/**
- * @param code the code to set
- */
-public void setCode(String code) {
-	this.code = code;
-}
-/**
- * @return the nom
- */
-public String getNom() {
-	return nom;
-}
-/**
- * @param nom the nom to set
- */
-public void setNom(String nom) {
-	this.nom = nom;
-}
-/**
- * @return the prix
- */
-public double getPrix() {
-	return prix;
-}
-/**
- * @param prix the prix to set
- */
-public void setPrix(double prix) {
-	this.prix = prix;
-}
+	/**
+	 * @param code
+	 *            Code litteral d'identification de la pizza représenté par
+	 *            l'instance courrante de Pizza.
+	 * @param nom
+	 *            Nom de la pizza représenté par l'instance courrante de Pizza.
+	 * @param prix
+	 *            Prix de la pizza représenté par l'instance courrante de Pizza.
+	 */
+	public Pizza(String code, String nom, double prix) {
+		this.id = Pizza.currentGlobalId;
+		Pizza.currentGlobalId++;
+		this.code = code;
+		this.nom = nom;
+		this.prix = prix;
+	}
+
+	@Override
+	public String toString() {
+		return this.getCode() + " -> " + this.getNom() + "(" + this.getPrix() + "\u20AC)";
+	}
+
+	/**
+	 * Getter.
+	 * 
+	 * @return the currentGlobalId
+	 */
+	public static int getCurrentGlobalId() {
+		return currentGlobalId;
+	}
+
+	/**
+	 * Setter.
+	 * 
+	 * @param currentGlobalId
+	 *            the currentGlobalId to set
+	 */
+	public static void setCurrentGlobalId(int currentGlobalId) {
+		Pizza.currentGlobalId = currentGlobalId;
+	}
+
+	/**
+	 * Getter.
+	 * 
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Setter.
+	 * 
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Getter.
+	 * 
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * Setter.
+	 * 
+	 * @param code
+	 *            the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * Getter.
+	 * 
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * Setter.
+	 * 
+	 * @param nom
+	 *            the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * Getter.
+	 * 
+	 * @return the prix
+	 */
+	public double getPrix() {
+		return prix;
+	}
+
+	/**
+	 * Setter.
+	 * 
+	 * @param prix
+	 *            the prix to set
+	 */
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
 
 }
